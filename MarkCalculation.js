be540f79c057964ca3c79ec513127b890b39b7b6
MayuriBadgujar <120549440+MayuriBadgujar@users.noreply.github.com>
2024-11-14 00:12:16 +0530

    Add files via upload

---
+++ MarkCalculation.js
@@ -0,0 +1,120 @@
+
+
+// "use strict";
+// document.getElementById("btn").onclick = function () {
+//   //  alert{ 'Hello World!' };
+//   var data1 = document.getElementById("x1").value;
+//   var data2 = document.getElementById("x2").value;
+//   var data3 = document.getElementById("x3").value;
+
+//   console.log(typeof data1);
+//   console.log(typeof data2);
+//   console.log(typeof data3);
+
+//   console.log(data1, data2, data3);
+
+//   var msg = " ";
+
+//   if (data1 == "" || data2 == "" || data3 == "") {
+//     msg = "Please enter all fields!";
+//   } else if (isNaN(data1) || isNaN(data2) || isNaN(data3)) {
+//     msg = "All fields should contain numeric values!";
+//   } else if (data1 < 0 || data2 < 0 || data3 < 0) {
+//     msg = "All fields should contain positive numeric values!";
+//   } else {
+//     var num1 = Number(data1);
+//     var num2 = Number(data2);
+//     var num3= Number(data3);
+
+//     var TotalMarks = num1+num2+num3 ;
+//     var Percentage =( TotalMarks/300)*100;
+//     var grade;
+//     if(Percentage<40){
+//             console.log("Failed");
+//     }
+//     else if(Percentage<50){
+//   grade=console.log("D");
+//     }
+//     else if(Percentage<60){
+//       grade=  console.log("C");
+//             }
+//             else if(Percentage<70){
+//          grade=       console.log("B");
+//                     }
+//                     else if(Percentage<80){
+//                 grade=        console.log("A");
+//                             }
+//                             else if(Percentage<90){
+//                        grade=         console.log("A+");
+//                                     }
+//                                     else{
+//                               grade=          console.log("O");
+//                                     }
+                                    
+                                
+                                                
+
+
+//     document.getElementById("y1").innerHTML = TotalMarks;
+//     document.getElementById("y2").innerHTML = Percentage;
+//     document.getElementById("y3").innerHTML = grade;
+//   }
+
+//   document.getElementById("result").innerHTML = msg;
+// };
+"use strict";
+document.getElementById("btn").onclick = function () {
+    var data1 = document.getElementById("x1").value;
+    var data2 = document.getElementById("x2").value;
+    var data3 = document.getElementById("x3").value;
+    var num1 = Number(data1);
+    var num2 = Number(data2);
+    var num3 = Number(data3);
+
+    var TotalMarks = num1 + num2 + num3;
+    var Percentage = (TotalMarks / 300) * 100;
+    var grade;
+
+    var msg = "";
+
+    if (data1 === "" || data2 === "" || data3 === "") {
+        msg = "Please enter all fields!";
+    } else if (isNaN(data1) || isNaN(data2) || isNaN(data3)) {
+        msg = "All fields should contain numeric values!";
+    } else if (data1 < 0 || data2 < 0 || data3 < 0) {
+        msg = "All fields should contain positive numeric values!";
+    } 
+    
+    else if(data1>=40  && data2>=40  && data3>=40){
+      
+        if (Percentage < 40) {
+            grade = "Failed";
+        } else if (Percentage < 50) {
+            grade = "D";
+        } else if (Percentage < 60) {
+            grade = "C";
+        } else if (Percentage < 70) {
+            grade = "B";
+        } else if (Percentage < 80) {
+            grade = "A";
+        } else if (Percentage < 90) {
+            grade = "A+";
+        } else {
+            grade = "O";
+        }
+
+        document.getElementById("y1").innerHTML = TotalMarks;
+        document.getElementById("y2").innerHTML = Percentage.toFixed(2); // to limit percentage to 2 decimal points
+        document.getElementById("y3").innerHTML = grade;
+    }
+    else{
+        document.getElementById("y1").innerHTML = TotalMarks;
+        document.getElementById("y2").innerHTML = Percentage.toFixed(2); // to limit percentage to 2 decimal points
+        document.getElementById("y3").innerHTML = grade;
+        
+console.log("failed");
+     
+    }
+
+    document.getElementById("result").innerHTML = msg;
+};
